8961dd6c4e04c35b66823811721fd62d069d0b3d
Jorge <jorgomez@protonmail.com>
2024-12-10 21:43:39 +0000

    “update”

--- src/api/models/skillRequest.js
+++ src/api/models/skillRequest.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 
 const skillRequestShema = new mongoose.Schema(
   {
-    // user: {
-    //   type: mongoose.Types.ObjectId,
-    //   ref: 'users',
-    //   required: true
-    // },
     user: {
-      type: String,
+      type: mongoose.Types.ObjectId,
+      ref: 'users',
       required: true
     },
+    // user: {
+    //   type: String,
+    //   required: true
+    // },
     picture: { type: String },
     skillToLearn: { type: String, required: true },
     skillToTeach: { type: String },
